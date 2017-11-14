62eab39e19553523fc93f0b3c44a66d3c029351f
Gorynya Anton <g84086619@china.huawei.com>
2017-11-14 10:56:43 +0300

    first version

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+This script monitor sites from fille.
+Check correct responce from server and domain date expired
+
+# How to Install
+
+Python 3 should be already installed. Then download this script directly:
+
+
+# Quickstart
+
+You can download and run it directly through console
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+check_sites_health.py urls.txt
+
+https://www.ya.ru is OK
+domain name https://www.ya.ru is paid
+
+https://www.mail.ru is OK
+domain name https://www.mail.ru is paid
+
+https://www.vk.com is OK
+domain name https://www.vk.com is paid
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,59 @@
+import requests
+import whois
+import tldextract
+from datetime import datetime
+import argparse
+
+
+MIN_DAY_COUNT = 30
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as check_urls:
+        urls = check_urls.readlines()
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    responce = requests.get(url=url)
+    if responce.status_code == requests.codes.ok:
+        return True
+    return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain = whois.whois(domain_name)
+    expire_date = domain['expiration_date']
+    cur_date = datetime.now()
+    if isinstance(expire_date, list):
+        time_delta = expire_date[0] - cur_date
+    else:
+        time_delta = expire_date - cur_date
+    if time_delta.days >= MIN_DAY_COUNT:
+        return True
+    return False
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='sites monitoring')
+    parser.add_argument('path', help='path to urls file')
+    return parser
+
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    args = parser.parse_args()
+    path_to_urls_file = args.path
+    check_urls = load_urls4check(path_to_urls_file)
+    for url in check_urls:
+        url = url.strip()
+        devided_url = tldextract.extract(url)
+        domain_name = devided_url.domain + '.' + devided_url.suffix
+        if is_server_respond_with_200(url):
+            print('\n{} is OK'.format(url))
+        else:
+            print('\n{} is down'.format(url))
+        if get_domain_expiration_date(domain_name):
+            print('domain name {} is paid'.format(url))
+        else:
+            print('{} expired soon'.format(url))
